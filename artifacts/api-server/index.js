a5ab2d025352b20886098541a144047fc633f5c7
Saswat Dixit <85784995+FireStormy1@users.noreply.github.com>
2026-06-17 09:46:50 +0530

    Update index.js

--- artifacts/api-server/index.js
+++ artifacts/api-server/index.js
@@ -11,7 +11,7 @@ const io = new Server(server, {
     methods: ["GET", "POST"]
   },
   path: "/socket.io",
-  transports: ["websocket", "polling"]
+  transports: ["polling","websocket"]
 });
 
 io.on("connection", (socket) => {
